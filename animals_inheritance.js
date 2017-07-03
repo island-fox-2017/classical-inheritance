5d07576a48b890306416e98b45dddc1efd3aa93d
pdvega <pdvegaa@gmail.com>
2017-07-03 15:27:58 +0700

    release 0

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,15 +1,35 @@
 "use strict"
 
 class Animal {
-  constructor(){
-    
+  constructor(num_legs=2, is_warm_blooded=true){
+    this.num_legs = num_legs
+    this.is_warm_blooded = is_warm_blooded
   }
 }
 
-class Frog extends Animal {
-  
+class Chicken extends Animal {
+  constructor(num_legs, is_warm_blooded){
+    super(num_legs, is_warm_blooded)
+  }
 }
 
-class Bat extends Animal {
-  
+class Cat extends Animal {
+  constructor(num_legs, is_warm_blooded){
+    super(num_legs, is_warm_blooded)
+  }
 }
+
+class Dog extends Animal {
+  constructor(num_legs, is_warm_blooded){
+    super(4, is_warm_blooded)
+  }
+}
+
+let animal1 = new Chicken(this.num_legs)
+console.log(`Hewan Ayam ${animal1.num_legs}, ${animal1.is_warm_blooded}`);
+
+let animal2 = new Cat(4)
+console.log(`Hewan Kucing ${animal2.num_legs}, ${animal2.is_warm_blooded}`);
+
+let animal3 = new Dog(4, false)
+console.log(`Hewan Anjing ${animal3.num_legs}, ${animal3.is_warm_blooded}`);

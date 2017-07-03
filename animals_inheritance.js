2036f70bd5f5619d9220bc014d1046e5d75de627
hariantara <hariantara.iputu@gmail.com>
2017-07-04 00:01:28 +0700

    masih ada kekurangan saat mau menampilkan hasil composition di akhir

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,52 @@
 "use strict"
+
+class Animal {
+  constructor(kaki) {
+    this.kaki = kaki;
+
+  }
+
+  get foot()
+  {
+    return this.kaki;
+  }
+
+  warnaBulu(color)
+  {
+    console.log(`Binatang ini memiliki ${this.kaki} kaki dan warna bulu ${color}`);
+  }
+}
+
+
+class Ayam extends Animal {
+  constructor(kaki, makanan) {
+    super(kaki);
+    this.makanan = makanan;
+    this.power = new Power(this.air);// composition
+  }
+
+  pelihara()
+  {
+    console.log((`Ayam adalah hewan berkaki ${this.kaki} yang makan ${this.makanan}` ));
+  }
+
+}
+
+class Power {
+  constructor(air) {
+    this.air = air;
+
+  }
+
+  waterBender()
+  {
+    console.log(`Ayam ini memiliki kekuatan yang bisa mengendalikan ${this.air}` );
+  }
+
+}
+
+var binatang = new Ayam(4, 'Beras');
+var superPower = new Power('air');
+binatang.warnaBulu('Hitam');
+binatang.pelihara();
+binatang.power.waterBender();

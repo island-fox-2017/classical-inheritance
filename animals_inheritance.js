6a3429c2428352aeeeba909fe1094edfe03dc9aa
tezaharsony <teza.harsony2303942gmail.com>
2017-07-06 20:03:21 +0700

    classical inheritance

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,29 @@
 "use strict"
+class Animal {
+  constructor(num_legs, is_warm_blooded, food) {
+    this.num_legs = num_legs
+    this.is_warm_blooded = is_warm_blooded
+    this.food = food
+  }
+}
+
+class Frog extends Animal {
+  constructor(num_legs, is_warm_blooded, food){
+    super(num_legs, is_warm_blooded,food)
+    this.Ulti = new Ulti()
+  }
+}
+
+class Ulti {
+  constructor() {}
+  use_laser_vision() {
+    return "im using dagon"
+  }
+  use_invisible() {
+    return "im using shadow blade"
+  }
+}
+
+let kodok = new Frog (4,"warm blood","bug")
+
+console.log(kodok.Ulti.use_laser_vision());

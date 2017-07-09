881fae66b80f81b592fcbcf1c4856df818f19ef6
Ahmad Nasikin <ahmadnasikin@Ahmads-MacBook-Pro.local>
2017-07-09 21:04:32 +0700

    Animal Inheritance

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,27 @@
 "use strict"
+
+class Hewan{
+  constructor(bergerak, bernapas) {
+    this.gerak = bergerak;
+    this.napas = bernapas;
+  }
+}
+
+class Ayam extends Hewan {
+  constructor(bergerak, bernapas, berparuh) {
+  super(bergerak, bernapas)
+    this.makan = berparuh
+  }
+}
+
+class kucing extends Hewan {
+  constructor(bergerak, bernapas, bergigi) {
+  super(bergerak, bernapas)
+    this.gigi = bergigi
+  }
+}
+
+let kucingh = new Hewan ("pake kaki","pake paru-paru")
+
+console.log(sapi.gerak);
+console.log(sapi.napas);

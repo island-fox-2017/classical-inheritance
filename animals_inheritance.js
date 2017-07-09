25ecce67bd90ae0aca1daef4cd0c9ec72be6f17c
Ahmad Nasikin <ahmadnasikin@Ahmads-MacBook-Pro.local>
2017-07-10 01:33:44 +0700

    Revisi

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,52 +1,48 @@
-"use strict"
+'use strict'
 
-class Hewan{
-  constructor(bergerak, jumlah_kaki, bernapas) {
-    this.gerak = bergerak;
+class Animal {
+  constructor(bergerak, jumlah_kaki, bernafas) {
+    this.gerak = bergerak
     this.num_kaki = jumlah_kaki || 4
-    this.napas = bernapas;
+    this.nafas = bernafas
+  }
+}
+
+class Kijang extends Animal {
+  constructor(bergerak, jumlah_kaki, bernafas, bertanduk) {
+    super(bergerak, jumlah_kaki, bernafas)
+    this.tanduk = bertanduk
+    this.superPower = new SuperPower()
+  }
+  detailKijang() {
+    return `Kijang bergerak dengan ${this.gerak}, jumlah kaki ${this.num_kaki}, bernafas dengan ${this.nafas}, bertanduk ${this.tanduk}, dan mempunyai ${this.superPower.kijang_power()}`
+  }
+}
+
+class Burung extends Animal {
+  constructor(bergerak, jumlah_kaki, bernafas, berparuh) {
+    super(bergerak, jumlah_kaki, bernafas)
+    this.paruh = berparuh
+    this.superPower = new SuperPower()
+  }
+  detailBurung() {
+    return `Burung elang bergerak dengan ${this.gerak}, jumlah kaki ${this.num_kaki}, bernafas dengan ${this.nafas}, berparuh ${this.paruh} dan mempunyai ${this.superPower.burung_power()} `
   }
 }
 
 class SuperPower {
-  constructor() {
-
+  constructor () {
   }
-  use_laser() {
-    return 'jalu tajam';
+  kijang_power() {
+    return 'kepala keras'
   }
-  kucing_power() {
-    return 'cakar kuat'
-  }
-}
-
-
-class Ayam extends Hewan {
-  constructor(bergerak, jumlah_kaki, bernapas, berparuh) {
-  super(bergerak, jumlah_kaki, bernapas)
-    this.paruh = berparuh
-    this.superPower = new SuperPower()
-  }
-  detailAyam() {
-    return `Ayam jago bergerak dengan ${this.gerak}, dan mempunyai ${this.num_kaki} kaki, berparuh ${this.paruh}, dan memiliki kekuatan ${this.superPower.use_laser()}`;
+  burung_power() {
+    return 'kecepatan terbang'
   }
 }
 
-class Kucing extends Hewan {
-  constructor(bergerak, jumlah_kaki, bernapas, bergigi) {
-  super(bergerak, jumlah_kaki, bernapas)
-    this.gigi = bergigi
-    this.superPower = new SuperPower()
-  }
-  detailKucing() {
-    return `Kucing bergerak dengan ${this.gerak}, dan mempunyai ${this.num_kaki} kaki, bernapas dengan${this.napas}, memiliki gigi ${this.gigi}, dan mempunyai ${this.superPower.kucing_power()} `;
-  }
-}
+let kijang = new Kijang('berjalan', '', 'paru-paru', 'kuat');
+let burung = new Burung('terbang', '2', 'paru-paru', 'tajam' )
 
-
-let kucing = new Kucing('kaki', '', 'paru-paru', 'tajam')
-let ayam = new Ayam('kaki', '2','paru-paru', 'pendek')
-
-
-console.log(ayam.detailAyam())
-console.log(kucing.detailKucing());
+console.log(kijang.detailKijang());
+console.log(burung.detailBurung());
